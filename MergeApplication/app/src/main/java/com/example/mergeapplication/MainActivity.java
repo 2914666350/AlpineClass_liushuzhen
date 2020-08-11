b059cb7ffd8ff0106ac62c6cf59a8f5aa0ed78dd
15541139895 <Liu2914666350@163.com>
2020-08-11 12:01:25 +0800

    实现按钮功能

--- MergeApplication/app/src/main/java/com/example/mergeapplication/MainActivity.java
+++ MergeApplication/app/src/main/java/com/example/mergeapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mergeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int number = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number++;
+                textView.setText(String.valueOf(number));
+            }
+        });
     }
 }
